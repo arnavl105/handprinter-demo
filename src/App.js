35cefcbcd86ec4a409d2b7435bfc4a33e271a940
Arnav Luthra <arnavl105@gmail.com>
2018-04-13 09:30:05 -0400

    added logo, created first component and basic app structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './img/logo_handprint.png'
+import Homepage from './components/homepage.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
+        <img src={logo} alt="logo"/>
+        <hr/>
+
+        <Homepage />
+        
+        <hr/>
+        <p>Footer goes here</p>
       </div>
     );
   }
---
+++ src/components/homepage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Homepage extends Component {
+
+	render(){
+		return(<p>Homepage content goes here</p>);
+	}
+}
+
+export default Homepage;
