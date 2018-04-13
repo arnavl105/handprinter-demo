8728508ce8436d00d988f982942124fdb3f4e795
Arnav Luthra <arnavl105@gmail.com>
2018-04-13 10:53:53 -0400

    added jumbotron, created base component for action cards

---
+++ src/components/actionCards.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ActionCards extends Component {
+
+	render() {
+		return(
+			<p>Action Cards go here</p>
+		);
+	}
+
+}
+
+export default ActionCards;
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import { Row, Jumbotron, Button } from 'react-bootstrap';
+import ActionCards from './actionCards.js';
+import treeBg from '../img/trees.jpg';
+
 
 class Homepage extends Component {
 
 	render(){
-		return(<p>Homepage content goes here</p>);
+
+		return(
+			<div className="homepage">
+				<Row>
+				<Jumbotron style={{ background: "url(" + treeBg + ")",
+								backgroundSize: "auto",
+								backgroundRepeat: "round" }}>
+					<div className="jumboText" style={{ backgroundColor: "rgb(255, 255, 255, 0.75)" }}>
+			          	<h2>Ensuring the</h2>
+			          	<h2><b>world is better</b> off</h2>
+			          	<h2><b>with us</b> than without us</h2>
+			          	<br />
+			          	<p>A <b>handprint</b> measures the <b>positive impacts</b> we can make, simply by changing the way we do things, at home, and at work.</p>
+		          	</div>
+				</Jumbotron>
+				</Row>
+
+				<Row>
+					<ActionCards />
+				</Row>
+			</div>
+		);
 	}
 }
 
